cb05d365506478a4824942eecfaae8726bf0a8fa
victor <>
2023-10-19 16:36:10 -0300

    Xpath
    Adicionado o Xpath para buscar diretamente nas classes

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ libxml_use_internal_errors(true);
 $dom = new DOMDocument();
 $dom->loadHTML($html);
 
+$xpath = new DOMXPath($dom);
+
 $getH4 = $dom->getElementsByTagName('h4');
+
 $h4 = '';
 
 foreach ($getH4 as $listaDeH4) {
@@ -16,5 +19,15 @@ foreach ($getH4 as $listaDeH4) {
     $h4 .= "Titulo: " . $listaDeH4->textContent . "<br> <br>";
   }
 };
+/*Pegando elementos em  */
+$class = "f-primary my-none paper-title";
 
-$idPost = $dom->getElementsByClassName('volume-info');
+$idPosts = $xpath->query("//*[@class='$class']");
+
+$idPost = '';
+
+foreach ($idPosts as $id) {
+  $idPost .= "Post: " . $id->textContent . "<br>";
+};
+
+echo $idPost;
