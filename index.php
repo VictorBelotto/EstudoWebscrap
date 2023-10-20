30057b6662f0bc5eaa850eb349456b87e224b0f9
victor <>
2023-10-20 15:04:30 -0300

    Install composer
    Nova estrutura para capturar os dados

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $html = file_get_contents('origin.html');
 libxml_use_internal_errors(true);
 
@@ -12,16 +13,37 @@ $xpath = new DOMXPath($dom);
 
 $cardPosts = $xpath->query("//a[@class='paper-card p-lg bd-gradient-left']")->item(0);
 
-$titleAtribute = $xpath->query(".//div[@class='authors']/span/@title", $cardPosts);
+$postId = $xpath->query(".//div[@class='volume-info']", $cardPosts)->item(0);
 
-/* for($i = 0; $i < count($titleAtribute); $i++) {
-  echo $titleAtribute->item($i)->nodeValue . "<br>";
-} */
-
-$tituloPost = $xpath->query(".//h4", $cardPosts)->item(0);
-
-$autoresPost = $xpath->query(".//span", $cardPosts)->item(0);
+$postTitle = $xpath->query(".//h4", $cardPosts)->item(0);
 
 $postType = $xpath->query(".//div[@class='tags mr-sm']", $cardPosts)->item(0);
 
-$postId = $xpath->query(".//div[@class='volume-info']", $cardPosts)->item(0);
+$postAuthors = $xpath->query(".//span", $cardPosts);
+
+$authorsInstituitions = $xpath->query(".//div[@class='authors']/span/@title", $cardPosts);
+
+
+
+
+function rowPost($id, $title, $type, $authors, $instituitions)
+{
+  $arrayAuthors = '';
+  $arrayInstituition = '';
+
+  for ($i = 0; $i < count($authors); $i++) {
+    $arrayAuthors .= '[' . $authors->item($i)->textContent . ']' . " ; ";
+  }
+  for ($i = 0; $i < count($instituitions); $i++) {
+    $arrayInstituition .= '[' . $instituitions->item($i)->textContent . ']' . " ; ";
+  }
+
+  echo "<b>ID do Post: </b>  $id->textContent " . '<br>' .
+    "<b>Titulo do Post:</b> $title->textContent " . '<br>' .
+    "<b>Tipo do Post:</b>  $type->textContent " . '<br>' .
+    "<b>Autores do Post:</b>  $arrayAuthors " . '<br>' .
+    "<b>Instituição do Post:</b>  $arrayInstituition " . '<br>' .
+    '___________________________________________________________________' . '<br>';
+}
+
+rowPost($postId, $postTitle, $postType, $postAuthors, $authorsInstituitions);
